802954115e850210da4e97f6146a94f7d129cba3
jodieivana <jodieivana2003@gmail.com>
2024-04-01 18:14:24 +0700

    initial commit

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,66 @@
+
+<header class="header_section">
+            <div class="container">
+               <nav class="navbar navbar-expand-lg custom_nav-container ">
+                  <a class="navbar-brand" href="index.html"><img width="200" src="home/images/logo.png" alt="#" /></a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""> </span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="product.html">Bookshelf</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Discover<span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="about.html">About</a></li>
+                              <li><a href="testimonial.html">Testimonial</a></li>
+                           </ul>
+                        </li>
+
+                        <form class="form-inline">
+                           <div class="input-group">
+                              <div class="input-group-prepend" style="margin-right: -10px;">
+                                    <span class="input-group-text" style="background-color: #fff; border: none; padding-left: 20px; border-radius: 30px 0px 0px 30px;">
+                                       <i class="fa fa-search" aria-hidden="true" style="color: #808080;"></i>
+                                    </span>
+                              </div>
+                              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="color: #152E7A; border: none; border-radius: 0px 30px 30px 0px;">
+                           </div>
+                        </form>
+
+                        @if (Route::has('login'))
+                            @auth
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">{{ Auth::user()->name }}<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="route('profile.edit')">PROFILE</a></li>
+                                    <li>
+                                        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">LOG OUT</a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            @else
+                            <li class="nav-item">
+                                <a class="btn btn-success" id="registercss" href="{{ route('register') }}">REGISTER</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-success" id="logincss" href="{{ route('login') }}">LOG IN</a>
+                            </li>
+
+                            @endauth
+                        @endif
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,102 @@
+<section class="slider_section ">
+            <div class="slider_bg_box">
+            </div>
+            <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+
+                  <div class="carousel-item active">
+                     <div class="container">
+                        <div class="row align-items-center">
+                              <div class="col-md-5 col-lg-6">
+                                 <div class="detail-box">
+                                    <h1>
+                                          <span>
+                                          Your Gateway to
+                                          </span>
+                                          <br>
+                                          Riveting Reads
+                                    </h1>
+                                    <p>
+                                    Welcome to our book review haven, where every page is a new adventure waiting to be discovered!
+                                    </p>
+                                    <div class="btn-box">
+                                          <a href="" class="btn1">
+                                             DISCOVER NOW!
+                                          </a>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="col-md-7 col-lg-6">
+                                 <img src="home/images/slide1.png" class="img-fluid" alt="Image">
+                              </div>
+                        </div>
+                     </div>
+                  </div>
+                  
+                  <div class="carousel-item ">
+                  <div class="container">
+                        <div class="row align-items-center">
+                              <div class="col-md-5 col-lg-6">
+                                 <div class="detail-box">
+                                    <h1>
+                                          <span>
+                                          Stay Updated with
+                                          </span>
+                                          <br>
+                                          Book News
+                                    </h1>
+                                    <p>
+                                    Stay updated with the latest releases, author interviews, and literary insights in our book news section!
+                                    </p>
+                                    <div class="btn-box">
+                                          <a href="" class="btn1">
+                                             BOOK NEWS
+                                          </a>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="col-md-7 col-lg-6">
+                                 <img src="home/images/slide2.png" class="img-fluid" alt="Image">
+                              </div>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="carousel-item">
+                  <div class="container">
+                        <div class="row align-items-center">
+                              <div class="col-md-5 col-lg-6">
+                                 <div class="detail-box">
+                                    <h1>
+                                          <span>
+                                          Check Out
+                                          </span>
+                                          <br>
+                                          Our Book Lists
+                                    </h1>
+                                    <p>
+                                    Explore our curated collection of captivating reads!
+                                    </p>
+                                    <div class="btn-box">
+                                          <a href="" class="btn1">
+                                             BOOK LISTS
+                                          </a>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="col-md-7 col-lg-6">
+                                 <img src="home/images/slide3.png" class="img-fluid" alt="Image">
+                              </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="container">
+                  <ol class="carousel-indicators">
+                     <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                     <li data-target="#customCarousel1" data-slide-to="1"></li>
+                     <li data-target="#customCarousel1" data-slide-to="2"></li>
+                  </ol>
+               </div>
+            </div>
+         </section>
