cc489987056df6c38078da835e20edaf3e040b6c
Vellyn Natalie <97731846+vellynnatalie@users.noreply.github.com>
2024-04-23 20:25:09 +0700

    Revert "Callista"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
     public function index()
     {
         return view ('home.userpage');
-        // return view ('admin.home');
     }
     
     public function redirect()
