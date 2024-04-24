24c54c770cb6933a74429418207093bf58f584ef
jodieivana <jodieivana2003@gmail.com>
2024-04-24 10:51:29 +0700

    rapikan booknews

--- resources/views/home/release.blade.php
+++ resources/views/home/release.blade.php
@@ -3,49 +3,54 @@
             <div class="row">
             <div class="releasediv">
                <div class="row newscont">
-                  <p class="headlinep">
+                  <p class="headlinep"  onclick="window.location.href='index.html';">
                      book news
                   </p>
                   <i class="fa fa-caret-right" aria-hidden="true"></i>
                </div>
                
                <div class="row newscont">
-                  <img src="home/images/news/news1.png" alt="news1" class="newssmall">
+                  <img src="home/images/news/news1.png" alt="news1" onclick="window.location.href='index.html';" class="newssmall">
                   <div class="column textboxt">
-                     <p class="lightp">
+                     <p class="lightp" onclick="window.location.href='index.html';">
                         nonfiction
                      </p>
-                     <p class="titlep">
+                     <p class="titlep" onclick="window.location.href='index.html';">
                         New Book by Jenny State Coming This Fall
                      </p>
                   </div>
                </div>
 
                <div class="row newscont">
-                  <img src="home/images/news/news2.png" alt="news1" class="newssmall">
+                  <img src="home/images/news/news2.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
                   <div class="column textboxt">
-                     <p class="lightp">
+                     <p class="lightp" onclick="window.location.href='index.html';">
                         fiction
                      </p>
-                     <p class="titlep">
+                     <p class="titlep" onclick="window.location.href='index.html';">
                      Alan Moore’s Final Comic Book Coming This Fall
                      </p>
                   </div>
                </div>
 
                <div class="row newscont">
-                  <img src="home/images/news/news3.png" alt="news1" class="newssmall">
+                  <img src="home/images/news/news3.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
                   <div class="column textboxt">
-                     <p class="lightp">
+                     <p class="lightp" onclick="window.location.href='index.html';">
                         fiction
                      </p>
-                     <p class="titlep">
+                     <p class="titlep" onclick="window.location.href='index.html';">
                         Longlist for 2024 Walter Scott Prize is Revealed
                      </p>
                   </div>
                </div>
             </div>
+
+
                <div id="verticle-line"></div>
+
+               
             </div>
+            
          </div>
       </section>
