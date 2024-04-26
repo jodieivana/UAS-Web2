0f8f30960c59df09bcb5bb39515f450e44779270
jodieivana <jodieivana2003@gmail.com>
2024-04-26 19:05:22 +0700

    finalize release.blade

--- resources/views/home/release.blade.php
+++ resources/views/home/release.blade.php
@@ -1,56 +1,116 @@
 <section class="arrival_section">
-         <div class="container">
-            <div class="row">
-            <div class="releasediv">
-               <div class="row newscont">
-                  <p class="headlinep"  onclick="window.location.href='index.html';">
-                     book news
-                  </p>
-                  <i class="fa fa-caret-right" aria-hidden="true"></i>
-               </div>
+   <div class="container releasecontent">
+      <div class="row">
+         <div class="releasediv ">
+            <div class="row newscont">
+               <p class="headlinep"  onclick="window.location.href='index.html';">
+                  book news
+               </p>
+               <i class="fa fa-caret-right" aria-hidden="true"></i>
+            </div>
                
-               <div class="row newscont">
-                  <img src="home/images/news/news1.png" alt="news1" onclick="window.location.href='index.html';" class="newssmall">
-                  <div class="column textboxt">
-                     <p class="lightp" onclick="window.location.href='index.html';">
-                        nonfiction
-                     </p>
-                     <p class="titlep" onclick="window.location.href='index.html';">
-                        New Book by Jenny State Coming This Fall
-                     </p>
-                  </div>
-               </div>
-
-               <div class="row newscont">
-                  <img src="home/images/news/news2.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
-                  <div class="column textboxt">
-                     <p class="lightp" onclick="window.location.href='index.html';">
-                        fiction
-                     </p>
-                     <p class="titlep" onclick="window.location.href='index.html';">
-                     Alan Moore’s Final Comic Book Coming This Fall
-                     </p>
-                  </div>
-               </div>
-
-               <div class="row newscont">
-                  <img src="home/images/news/news3.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
-                  <div class="column textboxt">
-                     <p class="lightp" onclick="window.location.href='index.html';">
-                        fiction
-                     </p>
-                     <p class="titlep" onclick="window.location.href='index.html';">
-                        Longlist for 2024 Walter Scott Prize is Revealed
-                     </p>
-                  </div>
+            <div class="row newscont">
+               <img src="home/images/news/news1.png" alt="news1" onclick="window.location.href='index.html';" class="newssmall">
+               <div class="column textboxt">
+                  <p class="lightp" onclick="window.location.href='index.html';">
+                     nonfiction
+                  </p>
+                  <p class="titlep" onclick="window.location.href='index.html';">
+                     New Book by Jenny State Coming This Fall
+                  </p>
                </div>
             </div>
 
+            <div class="row newscont">
+               <img src="home/images/news/news2.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
+               <div class="column textboxt">
+                  <p class="lightp" onclick="window.location.href='index.html';">
+                     fiction
+                  </p>
+                  <p class="titlep" onclick="window.location.href='index.html';">
+                     Alan Moore’s Final Comic Book Coming This Fall
+                  </p>
+               </div>
+            </div>
 
-               <div id="verticle-line"></div>
+            <div class="row newscont">
+               <img src="home/images/news/news3.png" alt="news1" class="newssmall" onclick="window.location.href='index.html';">
+               <div class="column textboxt">
+                  <p class="lightp" onclick="window.location.href='index.html';">
+                     fiction
+                  </p>
+                  <p class="titlep" onclick="window.location.href='index.html';">
+                     Longlist for 2024 Walter Scott Prize is Revealed
+                  </p>
+               </div>
+            </div>
+         </div>
 
-               
+            <div id="verticle-line"></div>
+
+         <div>
+            <div class="releasediv">
+               <div class="column newscont">
+                  <p class="headlinep date">
+                     feautured review
+                  </p>
+                  <p class="lightp date">
+                     february 20, 2024
+                  </p>
+               </div>
+
+               <div class="ftrframebk">
+                  <img src="{{ asset('home/images/bestseller/book1.png') }}" class="img-fluid img-bestseller ftrbook" alt="" onclick="window.location.href='index.html';">
+               </div>
+
+               <div class="column ">
+                  <p class="lightp" onclick="window.location.href='index.html';">
+                     fiction
+                  </p>
+                  <p class="titlep" onclick="window.location.href='index.html';">
+                     VERITY
+                  </p>
+                  <p class="lightp ftrauthor" onclick="window.location.href='index.html';">
+                     by colleen hoover
+                  </p>
+               </div>
+            </div>
+         </div>
+            
+            <div id="verticle-line1"></div>
+
+         <div class="releasediv booklistdiv">
+            <div class="row newscont adjlists">
+               <p class="headlinep"  onclick="window.location.href='index.html';">
+                  book lists
+               </p>
+               <i class="fa fa-caret-right" aria-hidden="true"></i>
             </div>
             
+            <div class="column">
+               <p class="titlep bklistisi" onclick="window.location.href='index.html';">
+                  14 Best March Books for Young Reader
+               </p>
+
+               <p class="titlep bklistisi" onclick="window.location.href='index.html';">
+                  20 Best Books to Read in March
+               </p>
+
+               <p class="titlep bklistisi" onclick="window.location.href='index.html';">
+                  Can’t-Miss Picture Books by Celebrity Authors
+               </p>
+
+               <p class="titlep bklistisi" onclick="window.location.href='index.html';">
+                  31 Black History Month Books for Young Readers
+               </p>
+
+               <p class="titlep bklistisi" onclick="window.location.href='index.html';">
+                  30 Hot Books Headed to the Screen
+               </p>
+            </div>
+
          </div>
-      </section>
+         
+      </div>
+   </div>
+</section>
