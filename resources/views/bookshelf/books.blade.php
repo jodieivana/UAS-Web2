3d3a801b8f85288ffcb662849d7de27b45e63f45
jodieivana <jodieivana2003@gmail.com>
2024-04-27 16:50:23 +0700

    bookshelf done

--- resources/views/bookshelf/books.blade.php
+++ resources/views/bookshelf/books.blade.php
@@ -7,147 +7,51 @@
                <hr class="linep">
             </div>
             
-            <div class="row releasecontent2">
-
-               <div class="releasediv">
-                  <div class="row newscont">
-                     <p class="headlinep vwrheadline"  onclick="window.location.href='index.html';">
-                        BIOGRAPHY & MEMOIR
-                     </p>
-                     <i class="fa fa-caret-right" aria-hidden="true"></i>
-                  </div>
-                     
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book1.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           BYRON: A LIFE IN TEN LETTERS
+            <table style="width:100%">
+               <tr class="jdltabel">
+                  <th>COVER</th>
+                  <th>BOOK INFO</th>
+                  <th>PROGRESS</th>
+                  <th>REVIEW</th>
+                  <th></th>
+               </tr>
+               <tr>
+                  <td><img src="{{ asset('home/images/bestseller/book3.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';"></td>
+                  <td>
+                     <div class="column textboxt2">
+                        <p class="lightp3" onclick="window.location.href='index.html';">
+                           FICTION
                         </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           ANDREW STAUFFER
-                        </p>
-                     </div>
-                  </div>
-
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book2.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           TAMING THE OCTOPUS
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           KYLE EDWARD WILLIAMS
-                        </p>
-                     </div>
-                  </div>
-
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book3.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
+                        <p class="titlep3" onclick="window.location.href='index.html';">
                            BATTLE FOR THE BIRD
                         </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
+                        <p class="lightp32" onclick="window.location.href='index.html';">
                            KURT WAGNER
                         </p>
                      </div>
-                  </div>
-               </div> 
-
-               <div id="verticle-line"></div>
-
-               <div class="releasediv ">
-                  <div class="row newscont">
-                     <p class="headlinep vwrheadline"  onclick="window.location.href='index.html';">
-                        MYSTERY & DETECTIVE
+                  </td>
+                  <td>
+                     <select name="readstatus" id="readstatus" class="readstatus classic">
+                        <option value="read">READ</option>
+                        <option value="toread">TO READ</option>
+                        <option value="reading">READING</option>
+                     </select>
+                  </td>
+                  <td>
+                     <p class="lightp32" onclick="window.location.href='index.html';">
+                        WRITE A REVIEW
                      </p>
-                     <i class="fa fa-caret-right" aria-hidden="true"></i>
-                  </div>
-                     
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book4.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           HARD GIRLS
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           J. ROBERT LENNON
-                        </p>
+                  </td>
+                  <td>
+                     <div class="btn-box2">
+                        <a href="" class="btn1">
+                           DELETE
+                        </a>
                      </div>
-                  </div>
+                  </td>
+               </tr>
 
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book5.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           GONE WITH THE WITCH
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           ANGELA M. SANDERS
-                        </p>
-                     </div>
-                  </div>
+            </table>
 
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book6.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           CRIME AND CHERRY PITS
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           AMANDA FLOWER
-                        </p>
-                     </div>
-                  </div>
-               </div> 
-
-               <div id="verticle-line1"></div>
-
-               <div class="releasediv nwrbawah">
-                  <div class="row newscont">
-                     <p class="headlinep vwrheadline"  onclick="window.location.href='index.html';">
-                        TEENS & YOUNG ADULT
-                     </p>
-                     <i class="fa fa-caret-right" aria-hidden="true"></i>
-                  </div>
-                     
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book7.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           AMBER'S WAY
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           GLORIA GALLOWAY
-                        </p>
-                     </div>
-                  </div>
-
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book8.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           THE BREAKUP TOUR
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           EMILY WIBERLY
-                        </p>
-                     </div>
-                  </div>
-
-                  <div class="row newscont">
-                     <img src="{{ asset('home/images/bestseller/book9.png') }}" class="img-fluid img-bestseller nwrbook" alt="" onclick="window.location.href='index.html';">
-                     <div class="column textboxt">
-                        <p class="titlep" onclick="window.location.href='index.html';">
-                           MY THROAT AN OPEN GRAVE
-                        </p>
-                        <p class="lightp2" onclick="window.location.href='index.html';">
-                           TORI BOVALINO
-                        </p>
-                     </div>
-                  </div>
-               </div> 
-
-            </div>
          </div>
       </section>
