27cbf27fcf1a56689be4c6157b2f56aef9c5a094
jodieivana <jodieivana2003@gmail.com>
2024-04-30 02:16:50 +0700

    perbaiki bug

--- resources/views/notification/notification.blade.php
+++ resources/views/notification/notification.blade.php
@@ -10,16 +10,16 @@
       <meta name="keywords" content="" />
       <meta name="description" content="" />
       <meta name="author" content="" />
-      <link rel="shortcut icon" href="home/images/favicon.png" type="">
+      <link rel="shortcut icon" href="{{ asset('home/images/favicon.png')}}" type="">
       <title>PageTurner - Your Gateway to Riveting Reads</title>
       <!-- bootstrap core css -->
-      <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+      <link rel="stylesheet" type="text/css" href="{{ asset('home/css/bootstrap.css')}}" />
       <!-- font awesome style -->
-      <link href="home/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="{{ asset('home/css/font-awesome.min.css')}}" rel="stylesheet" />
       <!-- Custom styles for this template -->
-      <link href="home/css/style.css" rel="stylesheet" />
+      <link href="{{ asset('home/css/style.css')}}" rel="stylesheet" />
       <!-- responsive style -->
-      <link href="home/css/responsive.css" rel="stylesheet" />
+      <link href="{{ asset('home/css/responsive.css')}}" rel="stylesheet" />
    </head>
    <body>
 
@@ -35,12 +35,12 @@
          </p>
       </div>
       <!-- jQery -->
-      <script src="home/js/jquery-3.4.1.min.js"></script>
+      <script src="{{ asset('home/js/jquery-3.4.1.min.js')}}"></script>
       <!-- popper js -->
-      <script src="home/js/popper.min.js"></script>
+      <script src="{{ asset('home/js/popper.min.js')}}"></script>
       <!-- bootstrap js -->
-      <script src="home/js/bootstrap.js"></script>
+      <script src="{{ asset('home/js/bootstrap.js')}}"></script>
       <!-- custom js -->
-      <script src="home/js/custom.js"></script>
+      <script src="{{ asset('home/js/custom.js')}}"></script>
    </body>
 </html>
